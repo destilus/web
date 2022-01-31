3c54131eae69598b36283ae86e685f1766535ed4
Leonardo Moura <leonardo.moura.barbosa@outlook.com>
2022-01-31 19:46:10 -0300

    refactor: change monorepo structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,34 @@
-const config = require('@repo/eslint-config')
-
-module.exports = config
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+}
