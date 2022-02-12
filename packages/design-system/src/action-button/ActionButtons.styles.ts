55edeac7c51e67af5f0abee131e601b89051abbf
Leo Silva <devleonardosilva@gmail.com>
2022-02-12 13:19:51 -0300

    Feat: Add action button component and post reactions

---
+++ packages/design-system/src/action-button/ActionButtons.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+  
+export const Container = styled.div`
+display: flex;
+align-items: center;
+flex-grow: 1;
+flex-basis: auto;
+justify-content: space-evenly;
+`;
---
+++ packages/front/components/post-reactions/post-reactions.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  height: 80vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 15%;
+  max-width: 500px;
+  background-color: #15202B; //cor fundo-twitter
+  color:#616F7C;
+  justify-content: space-between;
+  `;
